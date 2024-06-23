26e2259b0a418863fb9e7bab089e21a8a952fda3
sananakther6642 <sananakthar@gmail.com>
2024-06-24 01:25:25 +0200

    Added vr controller keys to navigate the presentation slides

--- StageFright_Light_VR/Assets/Scripts/Switch.cs
+++ StageFright_Light_VR/Assets/Scripts/Switch.cs
@@ -1,11 +1,15 @@
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.XR;
+using UnityEngine.InputSystem.Controls;
 
 public class Switch : MonoBehaviour
 {
     public Image[] backgroundCanvas1; // Array for images on the first canvas
     public Image[] backgroundCanvas2; // Array for images on the second canvas
     private int index;
+    private bool canNavigate = true;
 
     void Start()
     {
@@ -15,15 +19,61 @@ public class Switch : MonoBehaviour
 
     void Update()
     {
+        // Check for VR controller inputs
+        var leftHand = InputSystem.GetDevice<UnityEngine.InputSystem.XR.XRController>(CommonUsages.LeftHand);
+        var rightHand = InputSystem.GetDevice<UnityEngine.InputSystem.XR.XRController>(CommonUsages.RightHand);
+
+        if (leftHand != null)
+        {
+            HandleInput(leftHand);
+        }
+
+        if (rightHand != null)
+        {
+            HandleInput(rightHand);
+        }
+
         // Check for keyboard inputs
-        if (Input.GetKeyDown(KeyCode.RightArrow))
+        if (Keyboard.current.rightArrowKey.wasPressedThisFrame && canNavigate)
         {
             Next();
+            canNavigate = false;
+            Invoke(nameof(ResetNavigate), 0.5f); // Add a delay to prevent rapid navigation
         }
-        else if (Input.GetKeyDown(KeyCode.LeftArrow))
+        else if (Keyboard.current.leftArrowKey.wasPressedThisFrame && canNavigate)
         {
             Previous();
+            canNavigate = false;
+            Invoke(nameof(ResetNavigate), 0.5f); // Add a delay to prevent rapid navigation
         }
+    }
+
+    private void HandleInput(UnityEngine.InputSystem.XR.XRController controller)
+    {
+        // Assuming the VR controller has a joystick or thumbstick mapped to primary2DAxis
+        var primary2DAxis = controller.TryGetChildControl<Vector2Control>("primary2DAxis");
+
+        if (primary2DAxis != null)
+        {
+            Vector2 axis = primary2DAxis.ReadValue();
+            if (axis.x > 0.5f && canNavigate)
+            {
+                Next();
+                canNavigate = false;
+                Invoke(nameof(ResetNavigate), 0.5f); // Add a delay to prevent rapid navigation
+            }
+            else if (axis.x < -0.5f && canNavigate)
+            {
+                Previous();
+                canNavigate = false;
+                Invoke(nameof(ResetNavigate), 0.5f); // Add a delay to prevent rapid navigation
+            }
+        }
+    }
+
+    private void ResetNavigate()
+    {
+        canNavigate = true;
     }
 
     public void Next()
