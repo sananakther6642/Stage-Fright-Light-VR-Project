dcb0237931c23e966df699de7de9212c9d8b61e9
Hithesh C H <hitheshch2000@gmail.com>
2024-06-28 18:02:50 +0200

    Added logic in the code to save the file of time
    And check the console which display in the path for the file.

--- StageFright_Light_VR/Assets/Scripts/TrackingDirection.cs
+++ StageFright_Light_VR/Assets/Scripts/TrackingDirection.cs
@@ -1,5 +1,4 @@
-using System.Collections;
-using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 
 public class TrackingDirection : MonoBehaviour
@@ -45,6 +44,7 @@ public class TrackingDirection : MonoBehaviour
             Debug.Log("Looking elsewhere for: " + elsewhereLookTime + " seconds");
         }
     }
+
     // This method is called when the script is disabled
     void OnDisable()
     {
@@ -59,22 +59,39 @@ public class TrackingDirection : MonoBehaviour
         SaveTimes();
     }
 
-    // Method to save the look times to PlayerPrefs
+    // Method to save the look times to PlayerPrefs and to a file
     void SaveTimes()
     {
-        Debug.Log("Last");
+        // Create a file name with the current time
+        string fileName = "LookTimes_" + System.DateTime.Now.ToString("yyyyMMdd_HHmmss") + ".txt";
+        string filePath = Path.Combine(Application.persistentDataPath, fileName);
+
+        // Prepare the content to write
+        string content = "Looking at audience for: " + audienceLookTime + " seconds\n" +
+                         "Looking at slides Laptop for: " + slidesLookLaptopTime + " seconds\n" +
+                         "Looking at slides Beamer for: " + slidesLookBeamerTime + " seconds\n" +
+                         "Looking elsewhere for: " + elsewhereLookTime + " seconds";
+
+        // Write the content to the file
+        File.WriteAllText(filePath, content);
+
+        // Debug logs
+        Debug.Log("Saved look times to: " + filePath);
+        Debug.Log("Looking at audience for: " + audienceLookTime + " seconds");
+        Debug.Log("Looking at slides Laptop for: " + slidesLookLaptopTime + " seconds");
+        Debug.Log("Looking at slides Beamer for: " + slidesLookBeamerTime + " seconds");
+        Debug.Log("Looking elsewhere for: " + elsewhereLookTime + " seconds");
+
         // Save the audience look time
         PlayerPrefs.SetFloat("AudienceLookTime", audienceLookTime);
-        Debug.Log("Looking at audience for: " + audienceLookTime + " seconds");
-        // Save the slides look time
         PlayerPrefs.SetFloat("slidesLookLaptopTime", slidesLookLaptopTime);
-        Debug.Log("Looking at slides Laptop for: " + slidesLookLaptopTime + " seconds");
         PlayerPrefs.SetFloat("slidesLookBeamerTime", slidesLookBeamerTime);
-        Debug.Log("Looking at slides Beamer for: " + slidesLookBeamerTime + " seconds");
-        // Save the elsewhere look time
         PlayerPrefs.SetFloat("ElsewhereLookTime", elsewhereLookTime);
-        Debug.Log("Looking elsewhere for: " + elsewhereLookTime + " seconds");
+
         // Write the changes to disk
         PlayerPrefs.Save();
+
+        // Provide user with information on where to find the file
+        Debug.Log("You can find the look times file at: " + filePath);
     }
 }
